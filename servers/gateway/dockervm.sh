bc8046caedfbbf7d59498b58ed5d437235dd2b54
JT Anderson <JTAnderson7@hotmail.com>
2019-10-16 16:14:11 -0700

    [Add] more docker information

--- servers/gateway/dockervm.sh
+++ servers/gateway/dockervm.sh
@@ -2,4 +2,24 @@
 
 docker rm -f jtanderson7/assignment2
 docker pull jtanderson7/assignment2
-docker run -d -p 80:80 jtanderson7/assignment2
+
+export TLSCERT=/etc/letsencrypt/live/api.sauravkharb.me/fullchain.pem
+export TLSKEY=/etc/letsencrypt/live/api.sauravkharb.me/privkey.pem
+
+docker run -d \
+-p 443:443 \
+-v /etc/letsencrypt:/etc/letsencrypt:ro \
+-e TLSCERT=$TLSCERT \
+-e TLSKEY=$TLSKEY \
+jtanderson7/assignment2
+
+# docker run -d \
+# -p 443:443 \
+# -v /etc/letsencrypt/live/api.sauravkharb.me/:/build:ro \
+# -e TLSKEY=privkey.pem -e TLSCERT=cert.pem \
+# jtanderson7/assignment2;
+
+
+# etc/letsencrypt/live/api.sauravkharb.me/
+
+# https://api.sauravkharb.me/v1/summary?url=http://ogp.me
