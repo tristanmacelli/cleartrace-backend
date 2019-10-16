bc0300da1d39c0f7e35db997cd923e9364795bea
JT Anderson <JTAnderson7@hotmail.com>
2019-10-16 16:32:17 -0700

    [Add] TLS functionality  added

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -13,13 +13,13 @@ func main() {
 	address := os.Getenv("ADDR")
 	// Default address the server should listen on
 	if len(address) == 0 {
-		address = ":80"
+		address = ":443"
 	}
 	//get the TLS key and cert paths from environment variables
 	//this allows us to use a self-signed cert/key during development
 	//and the Let's Encrypt cert/key in production
-	// tlsKeyPath := os.Getenv("TLSKEY")
-	// tlsCertPath := os.Getenv("TLSCERT")
+	tlsKeyPath := os.Getenv("TLSKEY")
+	tlsCertPath := os.Getenv("TLSCERT")
 
 	// starting a new mux session
 	mux := http.NewServeMux()
@@ -27,8 +27,8 @@ func main() {
 
 	// logging server location or errors
 	log.Printf("server is listening at %s...", address)
-	log.Fatal(http.ListenAndServe(address, mux))
-	// log.Fatal(http.ListenAndServeTLS(address, tlsCertPath, tlsKeyPath, mux))
+	//log.Fatal(http.ListenAndServe(address, mux))
+	log.Fatal(http.ListenAndServeTLS(address, tlsCertPath, tlsKeyPath, mux))
 
 	/* To host server:
 	- change path until in folder with main.go in it
