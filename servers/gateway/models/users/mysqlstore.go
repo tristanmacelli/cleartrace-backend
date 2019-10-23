785532d4ca941b9f9233572945cf8167aa5acd50
Tristan6 <tristanmacelli@gmail.com>
2019-10-23 13:49:45 -0700

    [ADD] Update method with explicit trx handling and prepared statments

--- servers/gateway/models/users/mysqlstore.go
+++ servers/gateway/models/users/mysqlstore.go
@@ -17,44 +17,28 @@ type MysqlStore struct {
 }
 
 // A partially constructed sql query to use in getter functions
-const queryString = "SELECT * FROM users where"
+const queryString = "SELECT * FROM users WHERE"
 
 //NewMysqlStore creates data source name which can be used to connect to the user database
 func NewMysqlStore() *MysqlStore {
 	// See docker run command for env vars that define database name & password
 	dsn := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/insert-database-name-here", os.Getenv("MYSQL_ROOT_PASSWORD"))
-	// Uncomment this if we can use a persistent connection for all transactions
-	// db, _ := sql.Open("mysql", dsn)
+	// We are using a persistent connection for all transactions
+	db, _ := sql.Open("mysql", dsn)
 	return &MysqlStore{
 		dsn: dsn,
-		// Uncomment this if we can use a persistent connection for all transactions
-		// db:  db,
+		db:  db,
 	}
-}
-
-//OpenConnection opens a connection to the user database
-// Delete this function if we can use a persistent connection for all transactions
-//	i.e. we are storing the open connection in our MysqlStore struct
-// Keep this function if we must use a different connection for each individual transactions
-//  i.e. we are opening a connection in each of our CRUD functions
-func OpenConnection(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("error opening database: %v\n", err)
-		return nil, err
-	}
-	return db, nil
 }
 
 // GetBy is a helper method that resolves all
-func (ms *MysqlStore) GetBy(query string) (*User, error) {
+func (ms *MysqlStore) GetBy(query string, queryValue string) (*User, error) {
 
-	// TODO: Open connection to db
-	rows, err := ms.db.Query(query)
-	// TODO: Close connection to db
-	if err != nil {
-		return nil, err
-	}
+	// A prepared statement allows us to use bind values the (?)
+	// in the query parameter which helps avoid sql injection attacks
+	stmt, _ := ms.db.Prepare(queryString + query)
+	defer stmt.Close()
+	row := stmt.QueryRow(queryValue)
 	// May need to alter the capitalization of these variables to match what is in
 	// schema.sql or vice versa
 	var ID int64
@@ -64,9 +48,12 @@ func (ms *MysqlStore) GetBy(query string) (*User, error) {
 	var FirstName string
 	var LastName string
 	var PhotoURL string
-	for rows.Next() {
-		err = rows.Scan(&ID, &Email, &PassHash, &UserName, &FirstName, &LastName, &PhotoURL)
-		if err != nil {
+	err := row.Scan(&ID, &Email, &PassHash, &UserName, &FirstName, &LastName, &PhotoURL)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// there were no rows, but otherwise no error occurred
+			fmt.Printf("error getting user: %v\n", err)
+		} else {
 			return nil, err
 		}
 	}
@@ -83,20 +70,20 @@ func (ms *MysqlStore) GetBy(query string) (*User, error) {
 
 //GetByID returns the User with the given ID
 func (ms *MysqlStore) GetByID(id int64) (*User, error) {
-	query := queryString + " ID = " + strconv.FormatInt(id, 10)
-	return ms.GetBy(query)
+	query := " ID = ?"
+	return ms.GetBy(query, strconv.FormatInt(id, 10))
 }
 
 //GetByEmail returns the User with the given email
 func (ms *MysqlStore) GetByEmail(email string) (*User, error) {
-	query := queryString + " Email = " + email
-	return ms.GetBy(query)
+	query := " Email = ?"
+	return ms.GetBy(query, email)
 }
 
 //GetByUserName returns the User with the given Username
 func (ms *MysqlStore) GetByUserName(username string) (*User, error) {
-	query := queryString + " UserName = " + username
-	return ms.GetBy(query)
+	query := " UserName = ?"
+	return ms.GetBy(query, username)
 }
 
 //Insert inserts the user into the database, and returns
@@ -106,20 +93,21 @@ func (ms *MysqlStore) Insert(user *User) (*User, error) {
 	// Using ? markers for the values will defeat SQL
 	// injection attacks
 
-	// TODO: Open connection to db
-	tx, err := ms.db.Begin()
-	insq := "insert into users(email, passHash, username, firstname, lastname, photoURL) values (?,?,?,?,?,?)"
-	res, err := ms.db.Exec(insq, user.Email, user.PassHash, user.UserName,
+	// Open a reserved connection to make an individual transaction
+	tx, _ := ms.db.Begin()
+	stmt, _ := tx.Prepare("INSERT INTO users(email, passHash, username, firstname, lastname, photoURL) VALUES (?,?,?,?,?,?)")
+	defer stmt.Close()
+	res, err := stmt.Exec(user.Email, user.PassHash, user.UserName,
 		user.FirstName, user.LastName, user.PhotoURL)
 
 	if err != nil {
 		fmt.Printf("error inserting new row: %v\n", err)
+		// Close the reserved connection upon failure
 		tx.Rollback()
-		// TODO: Close connection to db
 		return nil, err
 	}
+	// Close the reserved connection upon success
 	tx.Commit()
-	// TODO: Close connection to db
 
 	//get the auto-assigned ID for the new row
 	id, err := res.LastInsertId()
@@ -128,31 +116,48 @@ func (ms *MysqlStore) Insert(user *User) (*User, error) {
 		return nil, err
 	}
 	fmt.Printf("ID for new row is %d\n", id)
+	// Get and return this new user
 	return ms.GetByID(id)
 }
 
 //Update applies UserUpdates to the given user ID
 //and returns the newly-updated user
 func (ms *MysqlStore) Update(id int64, updates *Updates) (*User, error) {
-	// TODO: Implement this function per the comment above
+
+	// Open a reserved connection to db to make an individual transaction
+	tx, _ := ms.db.Begin()
+	stmt, _ := tx.Prepare("UPDATE users SET (?, ?) WHERE ID = ?")
+	// This will close the prepared statement once Exec is called
+	defer stmt.Close()
+	_, err := stmt.Exec(updates.FirstName, updates.LastName, strconv.FormatInt(id, 10))
+	if err != nil {
+		fmt.Printf("error updating row: %v\n", err)
+		// Close the reserved connection upon failure
+		tx.Rollback()
+		return nil, err
+	}
+	// Close the reserved connection upon success
+	tx.Commit()
 	return nil, nil
 }
 
 //Delete deletes the user with the given ID
 func (ms *MysqlStore) Delete(id int64) error {
-	// TODO: Implement this function per the comment above
 
-	// TODO: Open connection to db
-	tx, err := ms.db.Begin()
-	_, err = ms.db.Exec("DELETE FROM users WHERE ID " + strconv.FormatInt(id, 10))
+	// Open a reserved connection to db to make an individual transaction
+	tx, _ := ms.db.Begin()
+	stmt, _ := tx.Prepare("DELETE FROM users WHERE ID = ?")
+	// This will close the prepared statement once Exec is called
+	defer stmt.Close()
+	_, err := stmt.Exec(strconv.FormatInt(id, 10))
 	if err != nil {
 		fmt.Printf("error deleting row: %v\n", err)
+		// Close the reserved connection upon failure
 		tx.Rollback()
-		// TODO: Close connection to db
 		return err
 	}
+	// Close the reserved connection upon success
 	tx.Commit()
-	// TODO: Close connection to db
 	return nil
 }
 
