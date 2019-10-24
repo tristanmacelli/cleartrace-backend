bef4f5249fbb83ecfa80ef214ac89832bddcdbd1
Tristan6 <tristanmacelli@gmail.com>
2019-10-23 17:19:07 -0700

    [ADD] tests for GetByEmail and GetByUsername

--- servers/gateway/models/users/mysqlstore_test.go
+++ servers/gateway/models/users/mysqlstore_test.go
@@ -38,3 +38,65 @@ func TestGetByID(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+func TestGetByEmail(t *testing.T) {
+	//MysqlStore represents a connection to our user database
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	columns := []string{"ID", "Email", "PassHash", "UserName", "FirstName", "LastName", "PhotoURL"}
+
+	mock.ExpectPrepare("SELECT \\* FROM users")
+	mock.ExpectQuery("SELECT \\* FROM users").
+		WithArgs("user@domain.com").
+		WillReturnRows(mock.NewRows(columns))
+
+	// passes the mock to our struct
+	var ms = MysqlStore{}
+	ms.db = db
+
+	// now we execute our method with the mock
+	if user, err := ms.GetByEmail("user@domain.com"); err != nil {
+		t.Errorf("was not expecting an error, but there was none")
+		fmt.Print(user)
+	}
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestGetByUsername(t *testing.T) {
+	//MysqlStore represents a connection to our user database
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	columns := []string{"ID", "Email", "PassHash", "UserName", "FirstName", "LastName", "PhotoURL"}
+
+	mock.ExpectPrepare("SELECT \\* FROM users")
+	mock.ExpectQuery("SELECT \\* FROM users").
+		WithArgs("Sam").
+		WillReturnRows(mock.NewRows(columns))
+
+	// passes the mock to our struct
+	var ms = MysqlStore{}
+	ms.db = db
+
+	// now we execute our method with the mock
+	if user, err := ms.GetByUserName("Sam"); err != nil {
+		t.Errorf("was not expecting an error, but there was none")
+		fmt.Print(user)
+	}
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
