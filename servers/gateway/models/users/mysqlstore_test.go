c415d32865b2e409eeec68eac42d3e331b821a35
Tristan6 <tristanmacelli@gmail.com>
2019-10-23 22:31:54 -0700

    [REMOVE] buggy prepared statements

--- servers/gateway/models/users/mysqlstore_test.go
+++ servers/gateway/models/users/mysqlstore_test.go
@@ -18,7 +18,6 @@ func TestGetByID(t *testing.T) {
 	defer db.Close()
 	columns := []string{"ID", "Email", "PassHash", "UserName", "FirstName", "LastName", "PhotoURL"}
 
-	mock.ExpectPrepare("SELECT \\* FROM users")
 	mock.ExpectQuery("SELECT \\* FROM users").
 		WithArgs("1").
 		WillReturnRows(mock.NewRows(columns))
@@ -29,7 +28,7 @@ func TestGetByID(t *testing.T) {
 
 	// now we execute our method with the mock
 	if user, err := ms.GetByID(1); err != nil {
-		t.Errorf("was not expecting an error, but there was none")
+		t.Errorf("there was an error, but we were not expecting one")
 		fmt.Print(user)
 	}
 
@@ -49,7 +48,6 @@ func TestGetByEmail(t *testing.T) {
 	defer db.Close()
 	columns := []string{"ID", "Email", "PassHash", "UserName", "FirstName", "LastName", "PhotoURL"}
 
-	mock.ExpectPrepare("SELECT \\* FROM users")
 	mock.ExpectQuery("SELECT \\* FROM users").
 		WithArgs("user@domain.com").
 		WillReturnRows(mock.NewRows(columns))
@@ -60,7 +58,7 @@ func TestGetByEmail(t *testing.T) {
 
 	// now we execute our method with the mock
 	if user, err := ms.GetByEmail("user@domain.com"); err != nil {
-		t.Errorf("was not expecting an error, but there was none")
+		t.Errorf("there was an error, but we were not expecting one")
 		fmt.Print(user)
 	}
 
@@ -80,7 +78,6 @@ func TestGetByUsername(t *testing.T) {
 	defer db.Close()
 	columns := []string{"ID", "Email", "PassHash", "UserName", "FirstName", "LastName", "PhotoURL"}
 
-	mock.ExpectPrepare("SELECT \\* FROM users")
 	mock.ExpectQuery("SELECT \\* FROM users").
 		WithArgs("Sam").
 		WillReturnRows(mock.NewRows(columns))
@@ -91,7 +88,7 @@ func TestGetByUsername(t *testing.T) {
 
 	// now we execute our method with the mock
 	if user, err := ms.GetByUserName("Sam"); err != nil {
-		t.Errorf("was not expecting an error, but there was none")
+		t.Errorf("there was an error, but we were not expecting one")
 		fmt.Print(user)
 	}
 
@@ -121,20 +118,23 @@ func TestInsert(t *testing.T) {
 	if err != nil {
 		fmt.Println("Error when generating user")
 	}
-	fmt.Println(&u)
 
 	mock.ExpectBegin()
-	mock.ExpectPrepare("INSERT INTO users(email, passHash, username, firstname, lastname, photoURL) VALUES (?,?,?,?,?,?)")
-	mock.ExpectExec("INSERT INTO users").
-		WithArgs(u)
+	mock.ExpectExec("INSERT INTO users\\(email, passHash, username, firstname, lastname, photoURL\\) VALUES \\(\\?,\\?,\\?,\\?,\\?,\\?\\)").
+		WithArgs(u.Email, u.PassHash, u.UserName, u.FirstName, u.LastName, u.PhotoURL).
+		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	// passes the mock to our struct
 	var ms = MysqlStore{}
 	ms.db = db
 
 	// now we execute our method with the mock
-	if user, err := ms.Insert(u); err != nil {
-		t.Errorf("was not expecting an error, but there was none")
-		fmt.Print(user)
+	if _, err := ms.Insert(u); err != nil {
+		t.Errorf("there was an error, but we were not expecting one")
+	}
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
