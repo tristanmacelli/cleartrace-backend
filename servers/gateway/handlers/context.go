947c79f79dd97a8b0987aec4148e0e9a4a5000dd
JT Anderson <JTAnderson7@hotmail.com>
2019-11-03 17:15:50 -0800

    Context

--- servers/gateway/handlers/context.go
+++ servers/gateway/handlers/context.go
@@ -1,8 +1,18 @@
 package handlers
 
-//TODO: define a handler context struct that
+import (
+	"assignments-Tristan6/servers/gateway/models/users"
+	"assignments-Tristan6/servers/gateway/sessions"
+)
+
+//Context TODO: define a handler context struct that
 //will be a receiver on any of your HTTP
 //handler functions that need access to
 //globals, such as the key used for signing
 //and verifying SessionIDs, the session store
 //and the user store
+type Context struct {
+	Key     string
+	User    users.Store
+	Session sessions.Store
+}
