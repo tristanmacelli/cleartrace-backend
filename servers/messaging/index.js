779004aceb212ae71bb992aa6e19bb4457a13ff4
sauravkh <skharb@uw.edu>
2019-11-12 15:17:22 -0800

    add index.js for microservice

---
+++ servers/messaging/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//require the express and morgan packages
+const express = require("express");
+const morgan = require("morgan");
+
+//create a new express application
+const app = express();
+
+const addr = process.env.ADDR || ":80";
+//split host and port using destructuring
+const [host, port] = addr.split(":");
+
+//add JSON request body parsing middleware
+app.use(express.json());
+//add the request logging middleware
+app.use(morgan("dev"));
