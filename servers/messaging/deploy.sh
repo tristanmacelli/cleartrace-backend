10f0a2ee3690eb2241862cd572f4131a22f483dd
Tristan6 <tristanmacelli@gmail.com>
2019-11-19 19:27:23 -0800

    [FIX] deploy script

--- servers/messaging/deploy.sh
+++ servers/messaging/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+cd tsc
+tsc --outDir ../
+cd -
 bash build.sh
-docker push jtanderson7/assignment2
+sudo docker push jtanderson7/assignment2
 chmod g+x ./dockervm.sh
-scp -i ~/.ssh/info441_api.pem ./dockervm.sh ec2-user@api.sauravkharb.me:./
-ssh -i ~/.ssh/info441_api.pem ec2-user@api.sauravkharb.me "bash ./dockervm.sh"
+sudo scp -i ~/.ssh/info441_api.pem ./dockervm.sh ec2-user@api.sauravkharb.me:./
+sudo ssh -i ~/.ssh/info441_api.pem ec2-user@api.sauravkharb.me "bash ./dockervm.sh"
