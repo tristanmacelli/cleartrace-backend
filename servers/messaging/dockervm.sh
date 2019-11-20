cd2b86dfeb0f4db23b18f8724ba8de1df4578629
Tristan6 <tristanmacelli@gmail.com>
2019-11-19 19:26:52 -0800

    [ADD] changes to fix messaging deploy

--- servers/messaging/dockervm.sh
+++ servers/messaging/dockervm.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-docker rm -f messaging
-docker rm -f messaging2
-docker pull jtanderson7/assignment2
+sudo docker rm -f messaging
+sudo docker rm -f messaging2
+sudo docker pull jtanderson7/assignment2
 
 export TLSCERT=/etc/letsencrypt/live/api.sauravkharb.me/fullchain.pem
 export TLSKEY=/etc/letsencrypt/live/api.sauravkharb.me/privkey.pem
 
-docker rm -f mongodb
-docker run --network messagingNetwork -d --name mongodb -v ~/data:/data/db mongo
+sudo docker rm -f mongodb
+sudo docker run --network=messagingNetwork -d --name mongodb -v ~/data:/data/db mongo
+# mongo localhost:27017 collectionCreation.sh (This would create channels & messages)
 
-docker run -d \
---network messagingNetwork \
+sudo docker run -d \
+--restart=unless-stopped \
+--network=messagingNetwork \
 --name messaging \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
 -e TLSCERT=$TLSCERT \
@@ -19,8 +21,9 @@ docker run -d \
 -e ADDR=5001 \
 jtanderson7/assignment2
 
-docker run -d \
---network messagingNetwork \
+sudo docker run -d \
+--restart=unless-stopped \
+--network=messagingNetwork \
 --name messaging2 \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
 -e TLSCERT=$TLSCERT \
