928b71a16c85e7524126ec510e7c2c098300b0ef
Tristan6 <tristanmacelli@gmail.com>
2019-11-21 12:01:53 -0800

    [CHANGE] network used by and names of all containers in gateway

--- servers/gateway/dockervm.sh
+++ servers/gateway/dockervm.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 docker rm -f gateway
-docker rm -f sessionRedisStore
-docker rm -f sqlUserStore
+# TODO: We should probably not be removing the redis & sql on every deploy
+docker rm -f userStore
+docker rm -f sessionStore
+echo "pulling newest version of gateway"
 docker pull jtanderson7/assignment2
 
 export TLSCERT=/etc/letsencrypt/live/api.sauravkharb.me/fullchain.pem
 export TLSKEY=/etc/letsencrypt/live/api.sauravkharb.me/privkey.pem
 
-sudo docker run --network=messagingNetwork \
---name sqlUserStore -d mysql/mysql-server
+echo "starting gateway"
+docker run --network=infrastructure \
+--name userStore -d mysql/mysql-server
 
-sudo docker run --network=messagingNetwork \
---name sessionRedisStore -d redis
+docker run --network=infrastructure \
+--name sessionStore -d redis
 
 docker run -d \
 -p 443:443 \
---network=messagingNetwork \
+--network=infrastructure \
 --name gateway \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
 -e TLSCERT=$TLSCERT \
@@ -24,8 +27,9 @@ docker run -d \
 -e MESSAGEADDR=messaging:5001,messaging2:5002 \
 -e SUMMARYADDR=summary:5050 \
 -e SESSIONKEY=sessionkeyrandom \
--e REDISADDR=sessionRedisStore:6379 \
--e DSN=sqlUserStore:3306 \
+-e DSN=userStore:3306 \
+-e REDISADDR=sessionStore:6379 \
 jtanderson7/assignment2
+echo "service refresh completed!"
 
 docker ps
