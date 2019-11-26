1a6fd1474f295caa16282375733add71a0cddb45
Saurav Kharb <saurav.kharb@hotmail.com>
2019-11-25 20:06:23 -0800

    Add starter code for web sockets

--- servers/gateway/handlers/websockets.go
+++ servers/gateway/handlers/websockets.go
@@ -1,10 +1,35 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// This function's purpose is to reject websocket upgrade requests if the
+		// origin of the websockete handshake request is coming from unknown domains.
+		// This prevents some random domain from opening up a socket with your server.
+		// TODO: make sure you modify this for your HW to check if r.Origin is your host
+		return true
+	},
+}
+
 //TODO: add a handler that upgrades clients to a WebSocket connection
 //and adds that to a list of WebSockets to notify when events are
 //read from the RabbitMQ server. Remember to synchronize changes
 //to this list, as handlers are called concurrently from multiple
 //goroutines.
+
+func (ctx *HandlerContext) WebSocketConnectionHandler(w http.ResponseWriter, r *http.Request) {
+	// problem getting Session State
+	if ctx.SessionStore == nil {
+		return
+	}
+}
 
 //TODO: start a goroutine that connects to the RabbitMQ server,
 //reads events off the queue, and broadcasts them to all of
