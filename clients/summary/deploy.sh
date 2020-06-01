49c8584be02201362b5c21d159f9ab467fd00016
Tree <tristanmacelli@gmail.com>
2020-06-01 12:17:28 -0700

    [FIX] refresh script

--- clients/summary/deploy.sh
+++ clients/summary/deploy.sh
@@ -1,7 +1,10 @@
-  
 #!/usr/bin/env bash
+
 bash build.sh
+
+echo "Deploying to EC2"
 docker push tristanmacelli/summaryclient
 chmod g+x ./refresh.sh
 
-ssh -i ~/.ssh/slack-clone-server.pem ec2-user@slack.client.tristanmacelli.com < 'bash -s' refresh.sh
+echo "Starting Summary Client"
+ssh -i ~/.ssh/slack-clone-server.pem ec2-user@slack.client.tristanmacelli.com 'bash -s' < refresh.sh
