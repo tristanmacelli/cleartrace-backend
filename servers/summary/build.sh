cb9d08bff9ce52c7c2b4ef7c705da8c0f47fbb9f
Tristan Macelli <tristanmacelli@gmail.com>
2023-02-10 11:18:07 -0600

    [REMOVE] references to summary deprecated service

--- servers/summary/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Building Go binary & Docker container"
-# This creates a go build named after this directory
-# CGO_ENABlED=0 will force go to build statically
-CGO_ENABLED=0 GOOS=linux go build .
-docker build -t tristanmacelli/summary .
-go clean 
--- servers/summary/go.mod
+++
@@ -1,5 +0,0 @@
-module server-side-mirror/servers/summary
-
-go 1.20
-
-require golang.org/x/net v0.6.0
